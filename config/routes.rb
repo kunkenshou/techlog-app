36649048a441ff8dcc667bafa22d085bbd630826
kunkenshou <kunkenshou@gmail.com>
2023-07-09 14:02:13 +0900

    Home#top のテストをリファクタ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
-Rails.application.routes.draw do
-  get 'home/top'
+  #Rails.application.routes.draw do
+  # get 'home/top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  Rails.application.routes.draw do
+  root 'home#top'
 end
