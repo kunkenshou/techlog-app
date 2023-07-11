1bf5789dbe4255b4aca49ef68fb76ba984a46802
kunkenshou <kunkenshou@gmail.com>
2023-07-12 08:14:28 +0900

    HomeのSystem SpecをHeadlessモードで実行するように変更

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
+#RSpec.describe 'Home', type: :system do
+#  before do
+#    driven_by(:rack_test)
+#  end
+
 RSpec.describe 'Home', type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+  before do # ここから追記
+    driven_by :selenium_chrome_headless
+  end # ここまで追記
 
   describe 'トップページの検証' do
     it 'Home#top という文字列が表示される' do
